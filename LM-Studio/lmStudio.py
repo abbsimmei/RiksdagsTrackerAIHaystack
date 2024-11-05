a90e82fe1d9505058b9794322952e6becb020036
Simon Meier <116065827+abbsimmei@users.noreply.github.com>
2024-11-05 10:46:18 +0100

    Memory in construction

--- LM-Studio/lmStudio.py
+++ LM-Studio/lmStudio.py
@@ -1,7 +1,24 @@
+##################################
+#
+# Joakim Tips: Ha en annan AI i början som sammanfattar / Förkortar JSON filen för att minimera antalet Tokens
+#
+#
+##################################
+
+
+
 # Example: reuse your existing OpenAI setup
 from openai import OpenAI
 import requests
 import json
+
+memory = ""
+
+##################################
+#                                #
+#          API Request           #
+#                                #
+##################################
 
 url = "https://data.riksdagen.se/dokument/HC023020.json"
 
@@ -14,6 +31,19 @@ def requestApi(url):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+##################################
+#                                #
+#            Memory              #
+#                                #
+##################################
+
+def chatMemoryFunc(memory, answer):
+    newMemory = "Du är en hjälpsam assisten som ska svara på frågor. Du kommer få kontext både på ett dokument och föredetta diskutioner och du ska använda detta för att ge bättre svar."
+
+    newMemory = "Test"
+
+    return newMemory
+
 apiAnswer = requestApi(url)
 print(apiAnswer)
 
@@ -23,18 +53,30 @@ client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
 
 while True:
+    print("---------------------------------------")
     question = input("Message to RiksdagsTracker GPT: ")
+    print("\n")
+
+    context = "Du är en hjälpsam assisten som ska svara på frågor angående detta JSON dokument: " + str(apiAnswer)
 
     completion = client.chat.completions.create(
     model="model-identifier",
     messages=[
-        {"role": "system", "content": "Du är en hjälpsam assisten som ska svara på frågor angående detta JSON dokument: " + str(apiAnswer)},
+        {"role": "system", "content": context},
         {"role": "user", "content": question}
     ],
     temperature=0.7,
     )
+    answerMessage = completion.choices[0].message
+    answerContent = answerMessage.content
+    print("#####################")
+    print("# RikdagsGPT Svarar #")
+    print("#####################")
+    print("\n")
+    print(answerContent)
+    print("\n")
+    print("---------------------")
 
-    print(completion.choices[0].message)
 
 
 

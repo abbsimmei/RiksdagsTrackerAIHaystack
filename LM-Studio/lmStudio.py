2402d502ab2ebf3b73b77fbcd4f2e4a829e23b7e
Simon Meier <116065827+abbsimmei@users.noreply.github.com>
2024-11-12 10:41:58 +0100

    Added Open AI

--- LM-Studio/lmStudio.py
+++ LM-Studio/lmStudio.py
@@ -9,6 +9,7 @@
 
 # Example: reuse your existing OpenAI setup
 from openai import OpenAI
+from bs4 import BeautifulSoup
 import requests
 import json
 
@@ -18,8 +19,8 @@ client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
 
 ##################################
-#                                #
-#          API Request           #
+#          (outdated)            #
+#           API JSON             #
 #                                #
 ##################################
 
@@ -53,9 +54,24 @@ def requestApiHtml(url):
         print(f"An error occurred: {e}")
 
 urlHtml = "https://data.riksdagen.se/dokument/HC023020.html"
-htmlApiAnswer = requestApiHtml(urlHtml)
-print (htmlApiAnswer)
+htmlApi = requestApiHtml(urlHtml)
+#print (htmlApi)
 
+# Function to remove tags
+def remove_tags(html):
+
+    # parse html content
+    soup = BeautifulSoup(html, "html.parser")
+
+    for data in soup(['style', 'script']):
+        # Remove tags
+        data.decompose()
+
+    # return data by retrieving the tag content
+    return ' '.join(soup.stripped_strings)
+
+apiHtmlAnswer = remove_tags(htmlApi)
+#print(apiHtmlAnswer)
 
 ##################################
 #                                #
@@ -64,13 +80,12 @@ print (htmlApiAnswer)
 ##################################
 
 def chatMemoryFunc(memory, jsonDocument):
-    newMemory = "Du är en hjälpsam assisten som ska svara på frågor. Du kommer få kontext både på ett dokument och föredetta diskutioner och du ska använda detta för att ge bättre svar. Här är JSON Dokumentet: (" + str(jsonDocument) + "). Och här kommer före detta frågor och svar: (" + str(memory) + ")"
+    newMemory = "Du är en hjälpsam och informativ assistent som ska svara på frågor baserat på innehållet i det givna dokumentet och tidigare diskussioner. Ditt främsta mål är att ge tydliga och faktabaserade svar som hämtas direkt från dokumentet och tidigare konversationer. All information som du presenterar ska vara strikt baserad på det som finns i det tillhandahållna dokumentet. Var noga med att ange att din information kommer från dokumentet och specificera vilka som har skrivit och ställt sig bakom dokumentet. Använd relevant kontext från tidigare frågor och svar för att ge djupare och mer precisa svar, när det är lämpligt. Avvik inte från informationen i dokumentet. Spekulativa eller hypotetiska svar ska undvikas. Här är Dokumentet: (" + str(jsonDocument) + "). Här är tidigare frågor och svar som du kan använda som kontext: (" + str(memory) + ")"
     return newMemory
 
 def questAns(previous, question, answer):
-    discussion = previous + " Nästa Fråga var: (" + question + "). Och ditt svar var: (" + answer + ")" 
+    discussion = previous + " Användarens nästa Fråga var: (" + question + "). Och assistentens svar var: (" + answer + ")" 
     return discussion
-
 
 ##################################
 #                                #
@@ -85,10 +100,10 @@ while True:
     print("\n")
 
     #context = "Du är en hjälpsam assisten som ska svara på frågor angående detta JSON dokument: " + str(apiAnswer)
-    context = chatMemoryFunc(memory, apiAnswer)
-    print("--------------")
-    print(context)
-    print("--------------")
+    context = chatMemoryFunc(memory, apiHtmlAnswer)
+    #print("--------------")
+    #print(context)
+    #print("--------------")
 
     completion = client.chat.completions.create(
     model="model-identifier",
@@ -96,17 +111,17 @@ while True:
         {"role": "system", "content": context},
         {"role": "user", "content": question}
     ],
-    max_completion_tokens = 100,
-    max_tokens = 100,
+    #max_completion_tokens = 100,
+    #max_tokens = 100,
     temperature=0.07,
     )
     answerMessage = completion.choices[0].message
     answerContent = answerMessage.content
 
     memory = questAns(memory, question, answerContent)
-    print("--------------")
-    print(memory)
-    print("--------------")
+    #print("--------------")
+    #print(memory)
+    #print("--------------")
 
     print("#####################")
     print("# RikdagsGPT Svarar #")
